352fecd35ed2dc1047e9bb40848ea1e0d1bfa138
Kyron Smith <kds060902@outlook.com>
2023-01-09 15:41:11 +0000

    feat: added navbar and header components, setup basic pages using browser router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Article from "./pages/article/Article";
+import Comments from "./pages/comments/Comments";
 
 function App() {
   return (
-    <div className="App">
-App
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/articles/:article_id" element={<Article />} />
+        <Route path="/articles/:article_id/comments" element={<Comments/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
